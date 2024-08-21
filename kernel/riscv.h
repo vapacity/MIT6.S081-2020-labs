83e0e4c4d18e68c67f51e048385267e10751581d
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-21 18:00:29 +0800

    backtrace completed

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -7,13 +7,7 @@ r_mhartid()
   return x;
 }
 
-static inline uint64
-r_fp()
-{
-  uint64 x;
-  asm volatile("mv %0, s0" : "=r" (x) );
-  retrun x;
-}
+
 // Machine Status Register, mstatus
 
 #define MSTATUS_MPP_MASK (3L << 11) // previous mode.
@@ -326,6 +320,13 @@ sfence_vma()
   asm volatile("sfence.vma zero, zero");
 }
 
+static inline uint64
+r_fp()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  retrun x;
+}
 
 #define PGSIZE 4096 // bytes per page
 #define PGSHIFT 12  // bits of offset within a page
