84207da6703f397740e716ec34bdc9ac639450d1
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-28 00:46:26 +0800

    Uthread switching between threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -41,20 +41,23 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
-  // is the key already present?
-  struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key)
-      break;
-  }
-  if(e){
-    // update the existing key.
-    e->value = value;
-  } else {
-    // the new is new.
-    insert(key, value, &table[i], table[i]);
-  }
+	// is the key already present?
+	struct entry *e = 0;
+	for (e = table[i]; e != 0; e = e->next) {
+		if (e->key == key)
+			break;
+	}
+        if(e){
+		// update the existing key.
+			e->value = value;
+		}
+        else {
+			//修改加锁
+			pthread_mutex_lock(&lock[i]);
+			// the new is new.
+			insert(key, value, &table[i], table[i]);
+			pthread_mutex_unlock(&lock[i]);
+		}
 }
 
 static struct entry*
